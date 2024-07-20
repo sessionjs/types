2afc2ea534831590c050b5fdfd55b3123b320727
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2024-07-21 01:41:28 +0400

    Add Avatar and DisplayName StorageKeys

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -5,6 +5,9 @@ export interface Storage {
   has(key: string): boolean | Promise<boolean>
 }
 
+/** This is not comperhensive list: it does not include dynamic keys like `message_hash:{hash}` and others. It should be used as hint, not a strict enum. */
 export enum StorageKeys {
   LastHashes = 'last_hashes',
+  Avatar = 'avatar',
+  DisplayName = 'display_name',
 }
